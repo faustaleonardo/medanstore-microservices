f2eba014dcb84f8f9eeef4b223733dda3f055b0e
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-04-27 10:17:40 +0700

    change dockerfile

--- web-deploy.sh
+++ web-deploy.sh
@@ -1,6 +1,6 @@
 SHA=$(git rev-parse HEAD)
 
-docker build -t faustaleonardo/medanstore-web:latest -t faustaleonardo/medanstore-web:$SHA -f ./web/Dockerfile.dev ./web
+docker build -t faustaleonardo/medanstore-web:latest -t faustaleonardo/medanstore-web:$SHA -f ./web/Dockerfile ./web
 
 docker push faustaleonardo/medanstore-web:latest
 docker push faustaleonardo/medanstore-web:$SHA
