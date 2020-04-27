9393b0c0c3038b9f207d6ab9cd7726d01ea76d8f
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-04-27 10:08:11 +0700

    add dockerignore

--- web-deploy.sh
+++ web-deploy.sh
@@ -1,8 +1,11 @@
 SHA=$(git rev-parse HEAD)
 
-docker build -t faustaleonardo/medanstore-web:latest -t faustaleonardo/medanstore-web:$SHA -f ./web/Dockerfile ./web
+docker build -t faustaleonardo/medanstore-web:latest -t faustaleonardo/medanstore-web:$SHA -f ./web/Dockerfile.dev ./web
 
 docker push faustaleonardo/medanstore-web:latest
 docker push faustaleonardo/medanstore-web:$SHA
+
+kubectl apply -f k8s/web.yaml 
+kubectl apply -f k8s/ingress-web.yaml
 
 kubectl set image deployments/web-deployment web=faustaleonardo/medanstore-web:$SHA
